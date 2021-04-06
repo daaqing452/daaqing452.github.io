4360bda9f1de40a422094d175277725d3a3c740c
Yiqin Lu <daaqing452@gmail.com>
2021-04-07 05:12:27 +0800

    update geometry_modeling

--- note_files/geometry_modeling/geometry_modeling.js
+++ note_files/geometry_modeling/geometry_modeling.js
@@ -87,4 +87,34 @@ var str_geometry_modeling = "\
 	<p> 与Gauss函数相比少一个\\(1/\\sigma\\) </p> \
 	<h3> 正则化 </h3> \
 	<p> 避免过拟合 </p> \
+	<h4> 岭回归正则项 </h4> \
+	$$ \\min_{\\bf W}{||{\\bf Y-WX}||^2}+\\mu||{\\bf W}||_2^2 $$ \
+	<h4> 稀疏正则化 </h4> \
+	<p> 解决冗余基函数（过完备） </p> \
+	$$ \\min_{\\bf W}{||{\\bf Y-WX}||^2}+\\mu||{\\bf W}||_0 $$ \
+	<p> 可以用于压缩：将数据\\(\\bf X\\)压缩为\\(\\bf Y\\) </p> \
+	<h3> 张量积空间 </h3> \
+	<p> 两个一元函数的基函数相互乘积 </p> \
+	<p> 二元二次函数基函数空间为：\\( \\{ 1,x,y,x^2,y^2,xy \\} \\) </p> \
+	<p> 一般张量积空间：\\( \\{ b_i(u)b_j(v) \\}, \\quad i,j\\in[0,n] \\) </p> \
+	<h3> 映射 </h3> \
+	$$ f:\\mathbb{R}^n\\rightarrow\\mathbb{R}^m $$ \
+	<p> 其中，\\(n\\)是本征维度，\\(n\\)是映射空间维度 </p> \
+	<table> \
+		<tr> <td class='table_padding20'> - </td> <td> 向量值函数： </td> <td> \\( f:\\mathbb{R}^1\\rightarrow\\mathbb{R}^m \\) </td> </tr> \
+		<tr> <td> - </td> <td> 参数曲线： </td> <td> \\( f:\\mathbb{R}^1\\rightarrow\\mathbb{R}^2或\\mathbb{R}^3 \\) </td> </tr> \
+		<tr> <td> - </td> <td> 参数曲面： </td> <td> \\( f:\\mathbb{R}^2\\rightarrow\\mathbb{R}^3 \\) </td> </tr> \
+	</table> \
+	<p> <img width='400px' src='note_files/geometry_modeling/mapping.png'/> </p> \
+	<h3> 参数化 </h3> \
+	<p> 给定平面上系列点\\(p_i=(x_i,y_i)\\)，得到参数曲线 </p> \
+	$$ \\left\\{ \\begin{align} x=x(t) \\\\ y=y(t) \\end{align} \\right. , \\quad t\\in[0,1] $$ \
+	<p> 将\\(t\\)参数化，对\\((t_i,x_i)\\)和\\(t_i,y_i\\)分别求解 </p> \
+	<table> \
+		<tr> <td class='table_padding20'> - </td> <td> 均匀参数化： </td> <td> \\(t_{i+1}-t_i=\\text{const.}\\)；\\(t_i=i\\) </td> </tr> \
+		<tr> <td> - </td> <td> 弦长参数化： </td> <td> \\( t_{i+1}-t_i=||p_{i+1}-p_i|| \\) </td> </tr> \
+		<tr> <td> - </td> <td> 中心参数化： </td> <td> \\( t_{i+1}-t_i=\\sqrt{||p_{i+1}-p_i||} \\) </td> </tr> \
+		<tr> <td> - </td> <td> Foley-Nielsen参数化： </td> <td> \\(t_{i+1}-t_i=||p_{i+1}-p_i||\\cdot\\left\( 1+\\frac{3}{2}\\frac{\\hat{\\alpha}_i||p_i-p_{i-1}||}{||p_i-p_{i-1}||+||p_{i+1}-p_i||}+\\frac{3}{2}\\frac{\\hat{\\alpha}_{i+1}||p_{i+1}-p_i||}{||p_{i+1}-p_i||+||p_{i+2}-p_{i+1}||} \\right\) \\) </tr> </tr> \
+		<tr> <td> </td> <td> </td> <td> 其中\\( \\alpha_i=\\angle_{(p_{i-1},p_i,p_{i+1})} \\)，\\( \\hat{\\alpha}_i=\\min{\\left\( \\pi-\\alpha_i, \\frac{\\pi}{2} \\right\)} \\) </td> </tr> \
+	</table> \
 ";
